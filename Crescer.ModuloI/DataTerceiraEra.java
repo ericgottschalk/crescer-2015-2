e022188c6ba33483ec35314cd29d3da7b43732fc
unknown <eric.gottschalk@NB-002690.cwinet.local>
2015-09-17 17:29:10 -0300

    Nova classe DataTerceiraEra

---
+++ Crescer.ModuloI/DataTerceiraEra.java
@@ -0,0 +1,19 @@
+
+public class DataTerceiraEra
+{
+    private int dia;
+    private int ano;
+    private int mes;
+    
+    public DataTerceiraEra(int dia, int mes, int ano)
+    {
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+    }
+    
+    public boolean ehBissexto()
+    {
+        return true;
+    }
+}
