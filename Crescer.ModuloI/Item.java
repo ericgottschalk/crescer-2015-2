c8d911e9b2d02d3cbd5cf0e08d5e4bf35d973902
unknown <eric.gottschalk@NB-002690.cwinet.local>
2015-09-18 17:33:59 -0300

    Adicionar classes Item e Inventario.

---
+++ Crescer.ModuloI/Item.java
@@ -0,0 +1,13 @@
+
+public class Item
+{
+    private String descricao;
+    private int quantidade;
+    
+    public Item(String desc, int quant)
+    {
+        this.descricao = desc;
+        this.quantidade = quant;
+    }
+}
+
