df6581fb30f2057bee91affa163170a816106f68
ericgottschalk <you@example.com>
2015-09-18 00:06:56 -0300

    Novos testes em DwarfTest

--- Crescer.ModuloI/DwarfTest.java
+++ Crescer.ModuloI/DwarfTest.java
@@ -52,6 +52,42 @@ public class DwarfTest
     }
     
     @Test
+    public void naoRecebeFlechaEGanhaDoisExp()
+    {
+        Dwarf d = new Dwarf("Anao", new DataTerceiraEra(1, 1, 1996));
+        
+        for(int i = 0; i < 2; i++)
+            d.receberFlechada();
+            
+        d.receberFlechada();
+        
+        assertEquals(90, d.getHp());
+        assertEquals(2, d.getExp());
+    }
+    
+    @Test
+    public void naoRecebeFlechaNemExpNomeSeixas()
+    {
+        Dwarf d = new Dwarf("Seixas", new DataTerceiraEra(1, 1, 1995));
+            
+        d.receberFlechada();
+        
+        assertEquals(110, d.getHp());
+        assertEquals(0, d.getExp());
+    }
+    
+    @Test
+    public void naoRecebeFlechaNemExpNomeMeireles()
+    {
+        Dwarf d = new Dwarf("Meireles", new DataTerceiraEra(1, 1, 1995));
+            
+        d.receberFlechada();
+        
+        assertEquals(110, d.getHp());
+        assertEquals(0, d.getExp());
+    }
+    
+    @Test
     public void dwarfMorre()
     {
         Dwarf d = new Dwarf("");
@@ -60,6 +96,28 @@ public class DwarfTest
             d.receberFlechada();
             
         assertEquals(Status.MORTO, d.getStatus());
+    }
+    
+    @Test
+    public void hpNaoFicaNegatiivo12Flechadas()
+    {
+        Dwarf d = new Dwarf("");
+        
+        for(int i = 0; i < 12; i++)
+            d.receberFlechada();
+            
+        assertEquals(0, d.getHp());
+    }
+    
+    @Test
+    public void hpNaoFicaNegatiivo100Flechadas()
+    {
+        Dwarf d = new Dwarf("");
+        
+        for(int i = 0; i < 100; i++)
+            d.receberFlechada();
+            
+        assertEquals(0, d.getHp());
     }
     
 }
