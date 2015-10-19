d377cba0b1bb8a35785a39c8ac021ca6e72ca685
unknown <eric.gottschalk@NB-002690.cwinet.local>
2015-10-19 13:34:01 -0200

    style: Melhorar identação.

--- src/modulo-03-web/dia-01/lista-js-basico.js
+++ src/modulo-03-web/dia-01/lista-js-basico.js
@@ -1,5 +1,5 @@
 /*
- * Faça uma função chamada daisyGame que receba por argumento o número de pétalas da margarida e retorne 'Love me' ou 'Love me not' 
+ * Faça uma função chamada daisyGame que receba por argumento o número de pétalas da margarida e retorne 'Love me' ou 'Love me not'
  */
 
 function daisyGame(n)
@@ -37,20 +37,20 @@ function maiorTexto(strArray)
 /*
  *	Faça uma função chamada imprime que receba dois parâmetros:
  *		*Um array de strings e uma função.
- *	Dentro da função imprime chame a função do segundo parâmetro para cada elemento do array. 
+ *	Dentro da função imprime chame a função do segundo parâmetro para cada elemento do array.
  */
 
 function escreveNome(nome)
 {
 	console.log('Olá querido instrutor: ', nome);
-} 
+}
 
 
 function imprime(array, f)
 {
 	if (typeof f !== 'function')
 		return 'JABULANI: TypeError: ' + typeof f + ' is not a function';
-	
+
 	for (var prop in array)
  	{
  		if (typeof array[prop] === 'string')
@@ -68,7 +68,7 @@ function fibo(n)
 	if (n == 0 || n == 1)
 		return n;
 
-    return fibo(n - 1) + fibo(n - 2);
+  return fibo(n - 1) + fibo(n - 2);
 }
 
 function fiboSum(n)
@@ -78,14 +78,14 @@ function fiboSum(n)
 	if (typeof n !== 'number')
 		return NaN;
 
-	for (var i = 0; i < n; i++)
+	for (var i = 1; i <= n; i++)
 		sum += fibo(i);
 
 	return sum;
 }
 
 /*
- * Faça uma função excelis que receba uma string que seja uma referência válida para uma coluna Excel e retorne o valor que representa aquela coluna. 
+ * Faça uma função excelis que receba uma string que seja uma referência válida para uma coluna Excel e retorne o valor que representa aquela coluna.
  */
 
 function excelis(str)
