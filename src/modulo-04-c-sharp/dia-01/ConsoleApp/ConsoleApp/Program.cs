bd0e2a4b94c0c8ca2edbdc9a247b717f933720ac
ericgottschalk <ericng04@hotmail.com>
2015-10-26 17:39:31 -0200

    feat(Program): refactor

--- src/modulo-04-c-sharp/dia-01/ConsoleApp/ConsoleApp/Program.cs
+++ src/modulo-04-c-sharp/dia-01/ConsoleApp/ConsoleApp/Program.cs
@@ -8,6 +8,13 @@ namespace ConsoleApp
 {
     class Program
     {
+        private const string ADICIONAR = "1";
+        private const string REMOVER_PORNOME = "2";
+        private const string REMOVER_PORNUMERO = "3";
+        private const string LISTAR = "4";
+        private const string LISTAR_ORDENADO = "5";
+        private const string SAIR = "6";
+
         public static void Menu()
         {
             Console.WriteLine("1 - Adicionar contato");
@@ -18,41 +25,57 @@ namespace ConsoleApp
             Console.WriteLine("6 - Sair\n");
         }
 
+        public static void AdicionarContato(Agenda agenda)
+        {
+            Console.WriteLine("Digite o nome:");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Digite o numero:");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            var contato = new Contato(nome, numero);
+            agenda.AdicionarContato(contato);
+        }
+
+        public static void RemoverContatoPorNome(Agenda agenda)
+        {
+            Console.WriteLine("Digite o nome: ");
+            string nomeRemover = Console.ReadLine();
+            agenda.RemoverContatos(nomeRemover);
+        }
+
+        public static void RemoverContatoPorNumero(Agenda agenda)
+        {
+            Console.WriteLine("Digite o numero: ");
+            int numRemover = Convert.ToInt32(Console.ReadLine());
+            agenda.RemoverContatos(numRemover);
+        }
+
+
         public static bool Escolha(Agenda agenda)
         {
             string enter = Console.ReadLine();
             switch (enter)
             {
-                case "1":
-                    Console.WriteLine("Digite o nome:");
-                    string nome = Console.ReadLine();
-                    Console.WriteLine("Digite o numero:");
-                    int numero = Convert.ToInt32(Console.ReadLine());
-                    var contato = new Contato(nome, numero);
-                    agenda.AdicionarContato(contato);
+                case ADICIONAR:
+                    AdicionarContato(agenda);
                     return false;
 
-                case "2":
-                    Console.WriteLine("Digite o nome: ");
-                    string nomeRemover = Console.ReadLine();
-                    agenda.RemoverContatos(nomeRemover);
+                case REMOVER_PORNOME:
+                    RemoverContatoPorNome(agenda);
                     return false;
 
-                case "3":
-                    Console.WriteLine("Digite o numero: ");
-                    int numRemover = Convert.ToInt32(Console.ReadLine());
-                    agenda.RemoverContatos(numRemover);
+                case REMOVER_PORNUMERO:
+                    RemoverContatoPorNumero(agenda);
                     return false;
 
-                case "4":
+                case LISTAR:
                     Console.WriteLine(agenda.Listar());
                     return false;
 
-                case "5":
+                case LISTAR_ORDENADO:
                     Console.WriteLine(agenda.ListarOrdenandoPorNome());
                     return false;
 
-                case "6":
+                case SAIR:
                     Console.WriteLine("Precione qualquer tecla para sair!");
                     return true;
 
