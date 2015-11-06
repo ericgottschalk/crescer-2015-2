39617da8ad781da2cba2033e8429d0c8f5616581
ericgottschalk <you@example.com>
2015-11-05 23:36:35 -0200

    Merge branch 'master' of https://github.com/ericgottschalk/crescer-2015-2

--- src/modulo-04-c-sharp/dia-06/Locadora/Locadora.Web.MVC/Properties/Settings.Designer.cs
+++ src/modulo-04-c-sharp/dia-06/Locadora/Locadora.Web.MVC/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Locadora.Web.MVC.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "12.0.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -37,11 +37,10 @@ namespace Locadora.Web.MVC.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=NB-002690\\SQLEXPRESS;Initial Catalog=Locadora;User ID=sa;Password=Cre" +
-            "scer$5")]
-        public string ddjkvhxivgjx {
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=ERIC\\SQLEXPRESS;Initial Catalog=Locadora;User ID=sa;Password=eric")]
+        public string LOCADORA1 {
             get {
-                return ((string)(this["ddjkvhxivgjx"]));
+                return ((string)(this["LOCADORA1"]));
             }
         }
     }
